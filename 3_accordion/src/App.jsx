2cd90e8c31bff18b3503aaebd5af01f5c5d9f672
Shravastee Thakur <123631680+shravastee-thakur@users.noreply.github.com>
2024-07-10 16:38:45 +0530

    Add files via upload

---
+++ 3_accordion/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+// import Accordian from './components/accordian'
+import Accordion from './Accordion'
+
+const App = () => {
+  return (
+    <div>
+      {/* <Accordian /> */}
+      <Accordion />
+    </div>
+  )
+}
+
+export default App
